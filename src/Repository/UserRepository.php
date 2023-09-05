95fd3e1a52597704a63cb8ead3fa55d394b22c95
Emile <emile00013@gmail.com>
2023-09-05 08:32:26 +0200

    Fixe majority of update issues + Auth

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Repository;
+
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+
+class UserRepository extends ServiceEntityRepository implements UserLoaderInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    public function loadUserByUsername(string $username): ?User
+    {
+        return $this->createQueryBuilder('u')
+        ->where('u.username = :username')
+        ->setParameter('username', $username)
+        ->getQuery()
+        ->getOneOrNullResult();
+    }
+
+}
