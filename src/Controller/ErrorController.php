ae2112010018c70e5ed895a8b656ecb5ffc2968a
Emile <emile00013@gmail.com>
2023-11-24 15:04:36 +0100

    Custom error page

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -12,7 +12,7 @@ class ErrorController extends AbstractController
     /**
      * @Route("/error404", name="error_404")
      */
-    public function show404Error(): Response
+    public function error404(): Response
     {
         return $this->render('bundles/TwigBundle/Exception/error404.html.twig');
     }
